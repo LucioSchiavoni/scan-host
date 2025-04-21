d84111e5bff406b421543fb9badf9375f6e7b128
Lucio B. Schiavoni Lafourcade <lucio.schiavoni@mec.local>
2025-04-21 09:41:43 -0300

    fix(): cors prod

--- core/middleware/cors.go
+++ core/middleware/cors.go
@@ -8,14 +8,12 @@ import (
 
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Establecer los encabezados CORS
-		w.Header().Set("Access-Control-Allow-Origin", config.FrontendDev)
+		w.Header().Set("Access-Control-Allow-Origin", config.FrontendProd)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept, Origin")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 
-		// Manejar la solicitud preflight OPTIONS
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
