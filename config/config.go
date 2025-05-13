14de50695121d227400125d0510021da22bd0ffd
Lucio B. Schiavoni Lafourcade <lucio.schiavoni@mec.local>
2025-05-13 13:24:00 -0300

    feat(): restart always docker-compose

--- config/config.go
+++ config/config.go
@@ -19,8 +19,9 @@ var (
 )
 
 func LoadConfig() {
-	err := godotenv.Load("/app/.env")
+	err := godotenv.Load()
 
+	//godotenv.Load("/app/.env")
 	if err != nil {
 		log.Fatal(" No se pudo cargar el archivo .env. Asegúrate de que exista y tenga las variables necesarias.")
 	}
